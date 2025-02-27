bc1bfb3755f590be98a3ed09af62a045c83a8722
Kadir AK <kadir.akx@gmail.com>
2025-02-27 15:00:23 +0300

    MainView ile ortak alandan veri bağlama eklendi

--- ViewModels/PhoneViewModel.cs
+++ ViewModels/PhoneViewModel.cs
@@ -11,16 +11,28 @@ namespace MVVM.ViewModels
 {
     public class PhoneViewModel : BindableBase
     {
-        private PhoneModel _phone;
-        public PhoneModel Phone
+        private PhoneModel[] _phones;
+        public PhoneModel[] Phones
         {
-            get => _phone;
-            private set => SetProperty(ref _phone, value);
+            get => _phones;
+            private set => SetProperty(ref _phones, value);
         }
 
         public PhoneViewModel()
         {
-            Phone = new PhoneModel("Samsung Galaxy S21 Ultra 5G", "Samsung", 999);
+            Phones = new PhoneModel[]
+            {
+                new PhoneModel("Xiaomi Redmi Note 13 Pro", "Xiaomi", 299),
+                new PhoneModel("Samsung Galaxy S22", "Samsung", 799),
+                new PhoneModel("Apple iPhone 14 Pro Max", "Apple", 1199),
+                new PhoneModel("OnePlus 11", "OnePlus", 699),
+                new PhoneModel("Google Pixel 7 Pro", "Google", 899),
+                new PhoneModel("Huawei P50 Pro", "Huawei", 999),
+                new PhoneModel("Sony Xperia 1 IV", "Sony", 1299),
+                new PhoneModel("Oppo Find X5 Pro", "Oppo", 1099),
+                new PhoneModel("Realme GT 2 Pro", "Realme", 599),
+                new PhoneModel("Asus ROG Phone 6", "Asus", 999)
+            };
         }
     }
 }
